1ab9b21b7f36a94765de239c668c77125c6babe6
lixxu <xuzenglin@gmail.com>
2015-03-16 10:21:49 +0800

    bug: search filter not fill value

--- pyldaplite/__init__.py
+++ pyldaplite/__init__.py
@@ -17,11 +17,7 @@ class PyLDAPLite(object):
         self.base_dn = kwargs.get('base_dn')
         self.search_scope = kwargs.get('search_scope') or DEFAULT_SEARCH_SCOPE
         self.retrive_attrs = kwargs.get('retrive_attrs', None)
-        self.version_no = kwargs.get('version', 3)
-
-    @property
-    def version(self):
-        return getattr(ldap, 'VERSION{}'.format(self.version_no))
+        self.version = kwargs.get('version', ldap.VERSION3)
 
     def connect(self, username, password):
         self.conn = ldap.initialize(self.server)
@@ -34,39 +30,46 @@ class PyLDAPLite(object):
 
     def search_by(self, filter_value, **kwargs):
         limit = kwargs.get('limit', 0)
-        self.result_id = self.conn.search(self.base_dn,
-                                          self.search_scope,
-                                          filter_value,
-                                          self.retrive_attrs,
-                                          )
+        result_id = self.conn.search(self.base_dn,
+                                     self.search_scope,
+                                     filter_value,
+                                     self.retrive_attrs,
+                                     )
         results = []
         while 1:
-            result_type, result_data = self.conn.result(self.result_id, 0)
-            if (result_data == []) or (limit and len(results) >= limit):
+            result_type, result_data = self.conn.result(result_id, 0)
+            if result_data == []:
                 break
-
-            if result_type == ldap.RES_SEARCH_ENTRY:
+            elif result_type == ldap.RES_SEARCH_ENTRY:
                 results.append(result_data)
+
+            if limit > 0 and len(results) >= limit:
+                break
 
         return results
 
     def search_name(self, name, limit=0):
-        return self.search_by(COMMON_FILTERS['name'], limit=limit)
+        return self.search_by(COMMON_FILTERS['name'].format(name), limit=limit)
 
     def search_email(self, email, limit=0):
-        return self.search_by(COMMON_FILTERS['email'], limit=limit)
+        return self.search_by(COMMON_FILTERS['email'].format(email),
+                              limit=limit)
 
     def search_mail(self, email, limit=0):
         return self.search_email(email, limit)
 
     def search_phone(self, phone, limit=0):
-        return self.search_by(COMMON_FILTERS['phone'], limit=limit)
+        return self.search_by(COMMON_FILTERS['phone'].format(phone),
+                              limit=limit)
 
     def search_mobile(self, mobile, limit=0):
-        return self.search_by(COMMON_FILTERS['mobile'], limit=limit)
+        return self.search_by(COMMON_FILTERS['mobile'].format(mobile),
+                              limit=limit)
 
     def search_number(self, number, limit=0):
-        return self.search_by(COMMON_FILTERS['number'], limit=limit)
+        return self.search_by(COMMON_FILTERS['number'].format(number),
+                              limit=limit)
 
     def search_display(self, display_name, limit=0):
-        return self.search_by(COMMON_FILTERS['display'], limit=limit)
+        return self.search_by(COMMON_FILTERS['display'].format(display_name),
+                              limit=limit)
